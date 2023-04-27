6a77780c19de5a41e21fcf249c174d97103481ef
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-27 16:06:15 -0700

    Add redirection for short URL to actual URL page after form submission

--- express_server.js
+++ express_server.js
@@ -45,6 +45,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+    res.redirect(longURL); 
+});
+
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const shortId = shortURL();
