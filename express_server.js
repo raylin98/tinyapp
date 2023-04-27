f2da1c43eab7dcb4c4a31f31c05e5bb5e9e890de
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-27 15:53:43 -0700

    Add redirection after shortURL submission

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-function generateRandomString() {
+const shortURL = function generateRandomString() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
   for (let i = 0; i < 6; i++) {
@@ -16,6 +16,7 @@ function generateRandomString() {
 }
 
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -45,8 +46,12 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  const shortId = shortURL();
+  urlDatabase[shortId] = longURL;
+  console.log(`Parsed request body: ${JSON.stringify(req.body)}`);
+  console.log(`New short URL ID: ${shortId}`);
+  res.redirect(`/urls/${shortId}`);
 });
 
 app.listen(PORT, () => {
