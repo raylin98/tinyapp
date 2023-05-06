07134f9c4c492d2e01bb2a99e26ad94590f93159
Raymond Lin <raymond.lin.7690@gmail.com>
2023-05-05 18:02:04 -0700

    Eslinted helpers.js

--- helpers.js
+++ helpers.js
@@ -1,29 +1,29 @@
 function generateRandomString() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < 6; i++) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-  };
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
-  const getUserByEmail = function(email, users) {
-    for (const user in users) {
-      if (users[user].email === email) {
-        return users[user];
-      }
+const getUserByEmail = function(email, users) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
     }
-    return null;
-  };
+  }
+  return null;
+};
 
- const urlsForUser = function(urlDatabase, userID) {
-    let userURLS = {};
-    for (let key in urlDatabase) {
-      if (urlDatabase[key].userID === userID) {
-        userURLS[key] = urlDatabase[key];
-      }
+const urlsForUser = function(urlDatabase, userID) {
+  let userURLS = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === userID) {
+      userURLS[key] = urlDatabase[key];
     }
-    return userURLS;
+  }
+  return userURLS;
 };
 
 module.exports = { generateRandomString, getUserByEmail, urlsForUser };
